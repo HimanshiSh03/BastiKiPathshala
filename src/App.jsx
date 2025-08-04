d8ba36202933a2e7cc54ffb2db422152986417f0
HimanshiSh03 <himanshi03dev@gmail.com>
2025-08-04 14:23:19 +0530

    Build ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Volunteer from './pages/Volunteer'
